1a3b091bcbd7a9f5db38603ee7280f294fe0dffc
Michał Molenda <michal@toke.pl>
2016-04-18 09:23:58 +0700

    Poprawka w migracji

--- database/migrations/2016_04_16_023713_create_users_floccs_table.php
+++ database/migrations/2016_04_16_023713_create_users_floccs_table.php
@@ -16,9 +16,9 @@ class CreateUsersFloccsTable extends Migration
         {
             $table->increments('id')->unsigned();
             $table->integer('user_id')->nullable()->default(null)->unsigned();
-            $table->string('email', 100)->nullable()->default(null);
             $table->timestamp('time')->useCurrent();
-            $table->integer('activity_id')->nullable()->default(null);
+            $table->string('email', 100)->nullable()->default(null);
+            $table->integer('activity_id')->nullable()->default(null)->unsigned();
             $table->text('place')->nullable()->default(null);
             $table->text('tribes')->nullable()->default(null);
 
@@ -26,12 +26,6 @@ class CreateUsersFloccsTable extends Migration
                 ->foreign('user_id')
                 ->references('id')
                 ->on('users')
-                ->onDelete('cascade')
-                ->onUpdate('cascade');
-            $table
-                ->foreign('activity_id')
-                ->references('id')
-                ->on('activities')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
         });
